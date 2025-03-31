2abebdfe8b07754e85e57df4973e803f4617578a
Nikorasu47 <normal47@live.fr>
2025-03-31 21:05:56 +0200

    first step

--- assets/script.js
+++ assets/script.js
@@ -12,13 +12,24 @@ let i = 0;
 /**slide */
 leftArrow.addEventListener('click',()=>{
 	let currentSlide = slide[i];
-	slide[i].style.transform = 'tranlateX(0%);'
+	currentSlide.style.transform = 'translateX(0%)';
 	i++;
 	
 	console.log(i)
 	console.log (currentSlide)
 
 	i= i%slide.length
+})
+
+rightArrow.addEventListener('click',()=>{
+	let currentSlide = slide[i];
+	currentSlide.style.transform = 'translateX(0%)';
+	i--;
+	
+	console.log(i)
+	console.log (currentSlide)
+
+	
 })
 
 
