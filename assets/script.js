2ffddb29691baf9ae1668a87f19f50a4b62b4c20
Nikorasu47 <normal47@live.fr>
2025-04-02 17:03:42 +0200

    ajout et modif commentaire

--- assets/script.js
+++ assets/script.js
@@ -12,29 +12,50 @@ let i = 0;
 /**slide */
 leftArrow.addEventListener('click',()=>{
 	let currentSlide = slide[i];
-	currentSlide.style.transform = 'translateX(0%)';
-	i++;
+	
+	
+	if (i<slide.length) {	
+		i++;
+	}else{
+		i=0
+	}
+
+	
+	if (i!==currentSlide){
+		currentSlide.style.transform = 'translateX(100%)';
+	}else {
+		currentSlide.style.transform = 'translateX(0%)';
+	}
+	
+	
 	
 	console.log(i)
 	console.log (currentSlide)
+	
 
-	i= i%slide.length
 })
 
 rightArrow.addEventListener('click',()=>{
 	let currentSlide = slide[i];
 	currentSlide.style.transform = 'translateX(0%)';
-	i--;
+	
 	
 	console.log(i)
 	console.log (currentSlide)
 
+	if (i>0) {
+		i--;
+	}else{
+		i=3
+	}
+
+	if (i!==currentSlide){
+		currentSlide.style.transform = 'translateX(-100%)';
+	}else {
+		currentSlide.style.transform = 'translateX(0%)';
+	}
 	
 })
-
-
-
-
 
 
 
